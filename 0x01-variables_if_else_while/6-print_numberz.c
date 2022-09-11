2919d2266b36bc50e66ee0b3782cdf5be978f679
ConfidenceM <confimun@gmail.com>
2022-09-11 13:00:53 -0700

    Betty Style

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/*
+ * main assign a random nummber to variable
+ * and print last of number stored
+ * return : always success
+ */
+int main(void)
+{ 
+	int ch;
+	
+for (ch = 48; ch <= 57; ch++)
+	{
+putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
