94d1e7320cd40e8b52c0e131b3f1c0a60a1f7cb0
ConfidenceM <confimun@gmail.com>
2022-09-13 12:12:00 -0700

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,17 @@
 
 /**
  * prints-alphabets x10 lowercase on a new line
+ * print alphabet_x10 void to 0
  */
 
 void print_alphabet_x10(void)
-{ 
+{
 	int j;
 	int x;
 
 	for (x = 0; x < 10; x++)
 
-		for (j = 'a';j <= 'z'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 			_putchar(j);
 
 	_putchar('\n');
