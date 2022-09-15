3af9fd8098a88bcecd388b7a1b9b8a24cc24d148
ConfidenceM <confimun@gmail.com>
2022-09-14 20:05:23 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign -> print sign based on condition
  * @n: argument passed
- * Return: 1, 0 -1
+ * Return:  0 
  */
 
 int print_sign(int n)
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-			return (1);
+	_putchar('+');
+	return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
-			return (-1);
+	_putchar('-');
+	return (-1);
 	}
 	else
 	{
-		_putchar('0');
-		return (0);
+        _putchar('0');
+	return (0);
 
 }
