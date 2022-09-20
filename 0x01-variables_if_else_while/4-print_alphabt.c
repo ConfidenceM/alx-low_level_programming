f613815911f7c1f2caec60428533e0dcfafce80c
ConfidenceM <confimun@gmail.com>
2022-09-20 05:26:59 -0700

    betty documentation

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
+
 #include <ctype.h>
+
 /**
- * main -program to print letters except q and e followed by a new line
- *
- * Return : retuen 0
- */
+
+*main - Program to print alphabet letters except q and e followed by new line
+
+*
+
+*Return: return 0
+
+*/
+
 int main(void)
+
 {
+
 	int letter;
 
+
+
 	for (letter = 'a'; letter <= 'z'; letter++)
+
 	{
+
 		if (letter == 'q' || letter == 'e')
+
+		{
+
 			continue;
+
+		}
+
 		putchar(letter);
-			putchar('\n');
-			return (0);
+
 	}
+
+	putchar('\n');
+
+	return (0);
+
 }
