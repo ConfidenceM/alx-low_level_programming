1a98808b00ff783976dcee5f81e9df4022229e97
ConfidenceM <confimun@gmail.com>
2022-09-21 16:26:34 -0700

    Review Code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
 *  _strcmp-Function which compare two strings and
 *@s1: first string
@@ -11,61 +9,34 @@
 *		returns negative number if s1 < s2
 *		returns positive number if s1 > s
 */
-
-
-
 int _strcmp(char *s1, char *s2)
-
 {
 
 	int i = 0, diff = 0;
 
-
-
 	while (1)
-
 	{
-
 		if (s1[i] == '\0' && s2[i] == '\0')
-
 			break;
 
 		else if (s1[i] == '\0')
-
 		{
-
 			diff = s2[i];
-
 			break;
-
 		}
-
 		else if (s2[i] == '\0')
-
 		{
-
 			diff = s1[i];
-
 			break;
-
 		}
 
 		else if (s1[i] != s2[i])
-
 		{
-
 			diff = s1[i] - s2[i];
-
 			break;
-
 		}
-
 		else
-
 			i++;
-
-
-
 	}
 
 	return (diff);
