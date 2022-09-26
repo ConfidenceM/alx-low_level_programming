c8770a6545582e467e8aa51a8489705868f08af4
ConfidenceM <confimun@gmail.com>
2022-09-26 12:11:56 -0700

    hot fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,28 +1,31 @@
 #include "main.h"
 
-/*
-*_memset-function fills
-* the first n bytes of the memory area
-* pointed to by s with the constant byte b
-*@s:target
-*@b: constant byte
-*@n:number of byte
-*Return: returns new value of target
-*/
+#include <string.h>
+
+
+
+/**
+ * _memset-this memory set function
+ * @s: string
+ * @b: a charcter
+ * @n: an integer
+ * Return:  a string
+ */
 
 char *_memset(char *s, char b, unsigned int n)
+
 {
-while (n)
 
-	{
-
-		s[n - 1] = b;
-	n--;
-
-	}
-
-	return (s);
+		unsigned int a;
 
 
+
+for (a = 0; a < n; a++)
+
+						s[a] = b;
+
+
+
+				return (s);
 
 }
