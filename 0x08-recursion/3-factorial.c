2f09554782cfbd5f0211f65a0ce1c28061bbe558
ConfidenceM <confimun@gmail.com>
2022-09-28 05:46:41 -0700

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,8 @@
 #include "main.h"
-
-
-
 /**
-
-* factorial - searchs for the factorial of a number
-
+* factorial-searchs for the factorial of a number
 * @n: the number to find the factorial of
-
-*
-
 * Return: the factorial of the number
-
 */
 
 
