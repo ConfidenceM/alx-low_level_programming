247dcd0ad28963598fabad9c7bd130fad375dc1b
ConfidenceM <confimun@gmail.com>
2022-10-03 15:16:59 -0700

    review code

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,17 +1,9 @@
 #include "main.h"
 
-
-
 /**
-
-*strtow - splits a stirng into words
-
+*strtow-splits a stirng into words
 *@str: string to be splitted
-
-*
-
 *Return: pointer to the array of splitted words
-
 */
 
 
@@ -102,20 +94,10 @@ return (NULL);
 
 }
 
-
-
-
-
 /**
-
-* num_words - counts the number of words in str
-
+* num_words-counts the number of words in str
 *@str: string to be used
-
-*
-
 *Return: number of words
-
 */
 
 int num_words(char *str)
@@ -124,78 +106,39 @@ int num_words(char *str)
 
 int i = 0, words = 0;
 
-
-
 while (i <= len(str))
 
-{
 
 if ((str[i] != ' ') && (str[i] != '\0'))
-
 {
-
 i++;
-
 }
-
 else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-
 {
-
 words += 1;
-
 i++;
-
 }
-
 else
-
 {
-
 i++;
-
 }
-
 }
-
 return (words);
-
 }
-
-
 
 /**
-
-* len - returns length of str
-
+* len-returns length of str
 *@str: string to be counted
-
-*
-
 * Return: length of the string
-
 */
-
-
-
 int len(char *str)
 
 {
-
 int len = 0;
-
-
-
 if (str != NULL)
-
 {
-
 while (str[len])
-
 len++;
-
 }
-
 return (len);
-
 }
